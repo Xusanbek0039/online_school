a125b965e5d559f8036c95890e1955227c1b65a3
Snouden0071 <144625437+bekzodsindorov1524@users.noreply.github.com>
2023-10-22 23:18:43 +0500

    a

--- config/settings.py
+++ config/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'school_255.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'school_255.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
